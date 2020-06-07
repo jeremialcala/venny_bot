4a6d15ba2b0503abc758f1a8680efe5cc0395ae6
Jeremi Alcala <jeremialcala@gmail.com>
2020-06-07 13:47:09 -0500

    Showing products by store. Fixing some errors

--- objects/__init__.py
+++ objects/__init__.py
@@ -180,12 +180,13 @@ class Store(Object):
 
 
 class Product(Object):
-    def __init__(self, _id=None, title=None, tags: list = None, store=None, options: list = None, image_url=None,
+    def __init__(self, _id=None, title=None, tags: list = None, store=None, price=None, options: list = None, image_url=None,
                  subtitle=None, buttons: list = None):
         self._id = _id
         self.title = title
         self.tags = tags
         self.store = store
+        self.price = price
         self.options = options
         self.image_url = image_url
         self.subtitle = subtitle
